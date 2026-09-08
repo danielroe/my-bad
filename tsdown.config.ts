a98356abda490995ebbc6126fbaae42d1755a6dd
Daniel Roe <daniel@roe.dev>
2026-09-08 11:27:15 +0000

    chore: use dts.generator

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { clientPlugin } from './scripts/client-plugin.ts'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/channel/index.ts', 'src/vite/index.ts', 'src/vite/client.ts', 'src/presets/index.ts', 'src/sinks/index.ts'],
-  dts: { oxc: true },
+  dts: { generator: 'oxc' },
   exports: { devExports: true },
   deps: { neverBundle: ['tiny-open'] },
   plugins: [clientPlugin()],
